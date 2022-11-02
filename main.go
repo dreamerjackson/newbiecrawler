c28c6627c7e9e3da1d0eddfcc63ebb1080655e0a
zhengjianxun <zhuimengshaonian04@gmail.com>
2022-11-02 10:21:24 +0800

    for range

--- main.go
+++ main.go
@@ -7,26 +7,32 @@ import (
 )
 
 func main() {
-	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
+	urls := []string{"https://www.thepaper.cn/", "https://www.douban.com/group/szsh"}
 
-	if err != nil {
-		fmt.Println("fetch url error:%v", err)
-		return
+	for _, url := range urls {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println("fetch url error:%v\n", err)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Error status code:%v\n", resp.StatusCode)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Println("read content failed:%v", err)
+			return
+		}
+
+		if len(body) < 4096 {
+			fmt.Println("get content failed:%v", "length body < 4096")
+		} else {
+			fmt.Println("body:", len(body))
+		}
 	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		fmt.Println("read content failed:%v", err)
-		return
-	}
-
-	fmt.Println("body:", string(body))
 }
