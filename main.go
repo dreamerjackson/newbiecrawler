d727ec887298f8a0c006faa020ef82746c3e8004
zhengjianxun <zhuimengshaonian04@gmail.com>
2022-11-06 22:23:43 +0800

    struct

--- main.go
+++ main.go
@@ -4,10 +4,33 @@ import (
 	"fmt"
 	"github.com/dreamerjackson/newbiecrawler/collect"
 	"regexp"
+	"strconv"
 )
 
+var autoRe = regexp.MustCompile(`<span class="pl"> 作者</span>:[\d\D]*?<a.*?>([^<]+)</a>`)
+var public = regexp.MustCompile(`<span class="pl">出版社:</span>([^<]+)<br/>`)
+var pageRe = regexp.MustCompile(`<span class="pl">页数:</span> ([^<]+)<br/>`)
+var priceRe = regexp.MustCompile(`<span class="pl">定价:</span>([^<]+)<br/>`)
+var scoreRe = regexp.MustCompile(`<strong class="ll rating_num " property="v:average">([^<]+)</strong>`)
+var intoRe = regexp.MustCompile(`<div class="intro">[\d\D]*?<p>([^<]+)</p></div>`)
+
+type Bookdetail struct {
+	BookName  string
+	Author    string
+	Publicer  string
+	Bookpages int
+	Price     string
+	Score     string
+	Into      string
+}
+
+func (b Bookdetail) String() string {
+
+	return "书籍名字:" + b.BookName + " 作者 :" + b.Author + " 出版社" + b.Publicer + " 书籍页数：" + strconv.Itoa(b.Bookpages) + " 价格：" + b.Price + " 得分" + b.Score + " \n简介:" + b.Into
+}
+
 func main() {
-	urls := []string{"https://book.douban.com"}
+	urls := []string{"https://book.douban.com/subject/36104107/"}
 	for _, url := range urls {
 		body, err := collect.GetByBrowserFetch(url)
 		if err != nil {
@@ -22,12 +45,28 @@ func main() {
 }
 
 func parseContent(content []byte) {
-	//<a href="/tag/科普" class="tag">科普</a>
-	re := regexp.MustCompile(`<a href="([^"]+)" class="tag">[^<]+</a>`)
+	bookdetail := Bookdetail{}
+	bookdetail.Author = ExtraString(content, autoRe)
+	page, err := strconv.Atoi(ExtraString(content, pageRe))
 
-	matches := re.FindAllSubmatch(content, -1)
-
-	for _, m := range matches {
-		fmt.Printf("url:%s\n", "https://book.douban.com"+string(m[1]))
+	if err == nil {
+		bookdetail.Bookpages = page
 	}
+	bookdetail.Publicer = ExtraString(content, public)
+	bookdetail.Into = ExtraString(content, intoRe)
+	bookdetail.Score = ExtraString(content, scoreRe)
+	bookdetail.Price = ExtraString(content, priceRe)
+	fmt.Println(bookdetail)
+}
+
+func ExtraString(contents []byte, re *regexp.Regexp) string {
+
+	match := re.FindSubmatch(contents)
+
+	if len(match) >= 2 {
+		return string(match[1])
+	} else {
+		return ""
+	}
+
 }
