504b2ce2cfca81c8d3807df155120ca3407c6071
zhengjianxun <zhuimengshaonian04@gmail.com>
2022-11-06 21:54:36 +0800

    regexp

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/dreamerjackson/newbiecrawler/collect"
+	"regexp"
 )
 
 func main() {
-	urls := []string{"https://www.thepaper.cn/", "https://www.douban.com/group/szsh"}
+	urls := []string{"https://book.douban.com"}
 	for _, url := range urls {
 		body, err := collect.GetByBrowserFetch(url)
 		if err != nil {
@@ -15,7 +16,18 @@ func main() {
 		if len(body) < 4096 {
 			fmt.Println("get content failed: length body < 4096")
 		} else {
-			fmt.Println("body:", len(body))
+			parseContent(body)
 		}
+	}
+}
+
+func parseContent(content []byte) {
+	//<a href="/tag/科普" class="tag">科普</a>
+	re := regexp.MustCompile(`<a href="([^"]+)" class="tag">[^<]+</a>`)
+
+	matches := re.FindAllSubmatch(content, -1)
+
+	for _, m := range matches {
+		fmt.Printf("url:%s\n", "https://book.douban.com"+string(m[1]))
 	}
 }
